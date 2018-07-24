d86b892ed114d515d061de0ef7926d280a448bf4
Camal Cakar <camal_cakar@yahoo.de>
2018-07-24 22:50:57 +0200

    Initialize repository

---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	Height = 5
+	Width  = 5
+)
+
+type Universe [][]bool
+
+func NewUniverse() Universe {
+	universe := make([][]bool, Height)
+
+	for row := range universe {
+		universe[row] = make([]bool, Width)
+	}
+
+	return universe
+}
+
+func SymbolMapping(universeRow []bool) string {
+	var mappedSymbols string
+
+	for i := 0; i < len(universeRow); i++ {
+		if universeRow[i] == true {
+			mappedSymbols += "*"
+		} else {
+			mappedSymbols += " "
+		}
+	}
+
+	return mappedSymbols
+}
+
+func Step(current, future Universe) {
+	for y := range current {
+		for x := range current[y] {
+			future[x][y] = current.Next(x, y)
+		}
+	}
+
+	current = future
+}
+
+func (u Universe) Show() {
+	for _, row := range u {
+		println(SymbolMapping(row))
+	}
+}
+
+func (u Universe) Seed() {
+	// Seed
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	for y := range u {
+		for x := range u[y] {
+			if rand.Intn(100) < 25 {
+				u[y][x] = true
+			} else {
+				u[y][x] = false
+			}
+		}
+	}
+}
+
+func (u Universe) Alive(x, y int) bool {
+	if x >= Width {
+		x %= Width
+	}
+
+	if y >= Height {
+		y %= Height
+	}
+
+	if x < 0 {
+		x += Width
+	}
+
+	if y < 0 {
+		y += Height
+	}
+
+	if u[y][x] == true {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (u Universe) Neighbours(x, y int) int {
+	var count int
+
+	for yRaise := -1; yRaise < 2; yRaise++ {
+		for xRaise := -1; xRaise < 2; xRaise++ {
+			if xRaise == 0 && yRaise == 0 {
+				continue
+			}
+
+			if u.Alive(x+xRaise, y+yRaise) == true {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+func (u Universe) Next(x, y int) bool {
+	count := u.Neighbours(x, y)
+
+	if count == 3 && u.Alive(x, y) == false {
+		return true
+	} else if u.Alive(x, y) == true && count == 2 || count == 3 {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	fmt.Println("\033[H\033[2J")
+	universe := NewUniverse()
+	universe.Seed()
+
+	tmp := NewUniverse()
+
+	for i := 0; i < 10; i++ {
+		universe.Show()
+		time.Sleep(time.Second)
+		fmt.Println("\033[H\033[2J")
+		Step(universe, tmp)
+		universe, tmp = tmp, universe
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func CreateUniverse(t *testing.T) Universe {
+	t.Helper()
+
+	return Universe{
+		{true, true, true, true, true},
+		{false, false, false, false, true},
+		{false, false, false, false, false},
+		{true, false, false, false, false},
+		{true, true, true, true, false},
+	}
+}
+
+func TestRightSeizedUniverse(t *testing.T) {
+	universe := NewUniverse()
+
+	receivedHeight := len(universe)
+	receivedWidth := len(universe[0])
+
+	if receivedHeight != Height || receivedWidth != Width {
+		t.Errorf("Expected %d as height but got %d. Expected %d as width but got %d", Height, receivedHeight, Width, receivedWidth)
+	}
+}
+
+func TestCorrectSymbolMapping(t *testing.T) {
+	universeRow := []bool{true, true, true, false, true}
+
+	mappedUniverse := SymbolMapping(universeRow)
+
+	if strings.Compare(mappedUniverse, "*** *") != 0 {
+		t.Errorf("Expected mapping of %s but got %s", "**  *", mappedUniverse)
+	}
+}
+
+func TestLivingCellWithinArray(t *testing.T) {
+	universe := CreateUniverse(t)
+
+	alive := universe.Alive(0, 0)
+
+	if alive != true {
+		t.Errorf("Expected that the cell at [0,0] is alive but it was dead")
+	}
+}
+
+func TestDeadCellWithinArray(t *testing.T) {
+	universe := CreateUniverse(t)
+
+	alive := universe.Alive(0, 1)
+
+	if alive == true {
+		t.Errorf("Expected that the cell at [0,0] is dead but it was alive")
+	}
+}
+
+func TestAliveCellPositiveBeyondOfArray(t *testing.T) {
+	universe := CreateUniverse(t)
+
+	alive := universe.Alive(Width, Height)
+
+	if alive != true {
+		t.Errorf("Expected that the cell at [0,0] is alive but it was dead")
+	}
+}
+
+func TestDeadCellNegativeBeyondOfArray(t *testing.T) {
+	universe := CreateUniverse(t)
+
+	alive := universe.Alive(-1, -1)
+
+	if alive == true {
+		t.Errorf("Expected that the cell at [0,0] is dead but it was alive")
+	}
+}
+
+func TestLivingNeighbours(t *testing.T) {
+	universe := CreateUniverse(t)
+
+	count := universe.Neighbours(4, 3)
+
+	if count != 3 {
+		t.Errorf("Expected 6 living neighbours but got %d", count)
+	}
+}
+
+func TestLessThanTwoLivingNeighbours(t *testing.T) {
+	universe := CreateUniverse(t)
+
+	alive := universe.Next(2, 2)
+
+	if alive == true {
+		t.Errorf("Expected that cell will be dead in the next generation but it will be alive")
+	}
+}
+
+func TestWithTwoLivingNeighbours(t *testing.T) {
+	universe := CreateUniverse(t)
+
+	alive := universe.Next(0, 3)
+
+	if alive != true {
+		t.Errorf("Expected that cell will be alive in the next generation but it will be dead")
+	}
+}
+
+func TestWithThreeLivingNeighbours(t *testing.T) {
+	universe := CreateUniverse(t)
+
+	alive := universe.Next(4, 3)
+
+	if alive != true {
+		t.Errorf("Expected that cell will be alive in the next generation but it will be dead")
+	}
+}
+
+func TestWithThreeLivingNeighboursAndDead(t *testing.T) {
+	universe := CreateUniverse(t)
+
+	alive := universe.Next(1, 1)
+
+	if alive != true {
+		t.Errorf("Expected that cell will be alive in the next generation but it will be dead")
+	}
+}
